b41159fad77b79f4f5b5aeb11bf8e55a01dcb45e
OTB\Isaac.Richardson <isaac.richardson@onthebeach.co.uk>
2023-12-20 12:37:19 +0000

    Initial graphql set up
    not yet federated

---
+++ astronauts.js
@@ -0,0 +1,7 @@
+// Subgraph/service
+const { ApolloServer, gql } = require("apollo-server");
+const { buildFederatedSchema } = require("@apollo/federation");
+const fetch = require("node-fetch");
+
+const port = 4001;
+const apiUrl = "http://localhost:3000";
